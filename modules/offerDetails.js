94f5b2f37871869ae0e865a7e9baa8462bcb7243
bolleboris <vdr@brothom.nl>
2012-05-07 15:09:03 +0300

    dataindex aangepast

--- modules/offerDetails.js
+++ modules/offerDetails.js
@@ -17,8 +17,8 @@ var OfferLedenStore = new Ext.data.GroupingStore({
 	   prettyUrls: false,
 	   url: './modules/api/getOfferMembers.php',
 	   api: {
-		  create: './modules/api/putOfferMembers.php',
-		  update: './modules/api/putOfferMembers.php',
+		  create: './modules/api/putOfferMembers.php', // @todo
+		  update: './modules/api/putOfferMembers.php', // @todo
 		  destroy: './modules/api/deleteOfferMember.php'
 	   }
 	})
@@ -55,12 +55,13 @@ var OfferStatusStore = new Ext.data.ArrayStore({
 	]
 });
 
-var OfferPersonRecord = Ext.data.Record.create([
-   {name: 'PersoonNr'},
-   {name: 'Naam'},
-   {name: 'Email'},
-   {name: 'Telefoon1'},
-   {name: 'Type'},
+var OfferResourceRecord = Ext.data.Record.create([
+   {name: 'AutoId'},
+   {name: 'Merk'},
+   {name: 'Bijnaam'},
+   {name: 'Kenteken'},
+   {name: 'Plaats'},
+   {name: 'Straat'},
    {name: 'OfferNr'}
 ]);
 
@@ -78,7 +79,7 @@ var OfferLedenTopBar = new Ext.Toolbar({			//Topbar voor main window
 		   handler: function() {
 			  showUserSearchBox(function(BMUNr, W4ANr) {
 				  console.log('Callback succeeded');
-				  OfferLedenStore.add(new OfferPersonRecord({
+				  OfferLedenStore.add(new OfferResourceRecord({
 					PersoonNr: BMUNr,
 					Naam: 'Wordt opgehaald',
 					Email: 'Wordt opgehaald',
@@ -103,7 +104,7 @@ var OfferLedenTopBar = new Ext.Toolbar({			//Topbar voor main window
 var OfferDetailWindowBottomBar = new Ext.Toolbar({
 items : [
 	'->',
-	{
+	/*{
 	//	iconCls : 'icon-arrow_refresh',
 		iconCls : 'icon-arrow_undo',
 		text: 'Herstel',
@@ -128,7 +129,7 @@ items : [
 			   scope:this
 			})
 		}
-	},'-',{
+	},'-',*/{
      	iconCls : 'icon-disk',
 		text: 'Opslaan',
 		handler: function(){
@@ -191,7 +192,7 @@ var OfferDetailForm = new Ext.FormPanel({
 	items: [{
         fieldLabel: 'OfferNr',
         name: 'OfferNr',
-		dataIndex: 'OfferNr',
+	dataIndex: 'OfferNr',
         readOnly: true
     },{
         fieldLabel: 'Verantwoordelijke',
@@ -237,18 +238,33 @@ var OfferLedenGrid = new Ext.grid.GridPanel({
 	frame:false,
 //	width: 350,
 	height: 390,
-	title: 'leden',
+	title: 'Resources',
 	store: OfferLedenStore,
 	loadMask: {msg:'Bezig met laden..'},
 	anchor: '100%',
 	tbar: OfferLedenTopBar,
 	columns: [
-		{header: "Persoon", width: 50, sortable: true, dataIndex: 'PersoonNr'},
-		{header: "Naam", width: 125, sortable: true, dataIndex: 'Naam'},
-		{header: "Email", width: 150, sortable: true, dataIndex: 'Email'},
-		{header: "Telefoon", width: 100, sortable: true, dataIndex: 'Telefoon1'},
-		{header: "Soort", width: 100, sortable: true, dataIndex: 'Type'}
-	]
+		{header: "AutoId", width: 60, sortable: true, dataIndex: 'AutoId'},
+		{header: "Merk", width: 100, sortable: true, dataIndex: 'Merk'},
+		{header: "Bijnaam", width: 125, sortable: true, dataIndex: 'Bijnaam', groupable: false},
+		{header: "Kenteken", width: 70, sortable: true, dataIndex: 'Kenteken', groupable: false},
+		{header: "Plaats", width: 80, sortable: true, dataIndex: 'Plaats'},
+		{header: "Straat", width: 100, sortable: true, dataIndex: 'Straat', groupable: false},
+		{header: "Supplier", width: 100, sortable: true, dataIndex: 'Supplier'}
+	],
+	view: new Ext.grid.GroupingView({
+		forceFit: true,
+		startCollapsed: true,
+		columnsText: 'Toon kolommen',
+		groupByText: 'Groepeer op dit veld',
+		showGroupsText: 'Groepeer',
+		sortAscText: 'Sorteer oplopend',
+		sortDescText: 'Sorteer aflopend',
+		groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "Autos" : "Auto"]})'
+	}),
+	sm: new Ext.grid.RowSelectionModel({
+		singleSelect: true
+	})
 });
 
 var OfferDetailWindow = new Ext.Window({
@@ -274,12 +290,12 @@ var OfferDetailWindow = new Ext.Window({
 	bbar: OfferDetailWindowBottomBar
 });
 
-var showOfferDetails = function(OfferNr){
+var showOfferDetails = function (OfferNr, VerschafferNr) {
 	OfferDetailWindow.show(this);
-	OfferDetailWindow.setTitle("Offer "+OfferNr);
+	OfferDetailWindow.setTitle("Offer " + OfferNr + " details");
 	OfferDetailWindowNumber = OfferNr;
 	OfferSoortStore.load();
 
-	OfferLedenStore.load({params: {OfferNr : OfferNr}, waitMsg:'Laden...'});
+	OfferLedenStore.load({params: {offer_id : OfferNr, supplier_id: VerschafferNr}, waitMsg:'Laden...'});
 	OfferDetailForm.getForm().load({url:'./modules/api/getOffer.php', params: {OfferNr : OfferNr}, waitMsg:'Laden...'});
 };
